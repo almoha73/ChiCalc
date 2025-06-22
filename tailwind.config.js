9547fb2b662c5be7316b4cf2321e6e6b08f7db71
almoha73 <39214808+almoha73@users.noreply.github.com>
2025-06-22 19:04:48 +0200

    Added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        "palette-1": "#321114",
+        "palette-2": "#6b3f24", 
+        "palette-3": "#a49837",
+        "palette-4": "#a0c95e",
+        "palette-5": "#a0dd98"
+      }
+    },
+  },
+  plugins: [],
+};
